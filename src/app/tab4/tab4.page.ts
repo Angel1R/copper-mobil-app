588a66b3d5cb7b8f242179ceaed3775f5accae29
Angel1R <angelrodrigo.ar55@gmail.com>
2025-06-27 16:29:24 -0600

    Simulación del módulo de consumo de datos (Tab4)

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -4,10 +4,43 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
-  standalone:false
+  standalone: false
 })
-export class Tab4Page {
+export class Tab4Page implements OnInit {
+  consumoMB = 0;
+  limiteMB = 5000; // 5 GB = 5000 MB
+  porcentaje = 0;
+  advertencia = false;
+  agotado = false;
 
-  constructor() {}
- 
+  ngOnInit() {
+    this.actualizarEstado();
+  }
+
+  consumir() {
+    if (this.agotado) return;
+
+    const paso = 500; // Simula 250 MB por clic
+    this.consumoMB += paso;
+
+    if (this.consumoMB >= this.limiteMB) {
+      this.consumoMB = this.limiteMB;
+      this.agotado = true;
+    }
+
+    this.actualizarEstado();
+  }
+
+  actualizarEstado() {
+    const restante = this.limiteMB - this.consumoMB;
+    this.porcentaje = (this.consumoMB / this.limiteMB) * 100;
+    this.advertencia = restante <= 1024 && !this.agotado; // Si queda 1 GB o menos
+  }
+
+  recargarDatos() {
+    this.consumoMB = 0;
+    this.advertencia = false;
+    this.agotado = false;
+    this.actualizarEstado();
+  }
 }
