3150d34df4b9c4735ed09514d8f305a2b2d28370
Angel1R <angelrodrigo.ar55@gmail.com>
2025-07-11 20:53:16 -0600

    Actualizacion del Menu

--- src/app/menu-pages/perfil/perfil.page.ts
+++ src/app/menu-pages/perfil/perfil.page.ts
@@ -8,9 +8,25 @@ import { NavController } from '@ionic/angular';
   standalone:false
 })
 export class PerfilPage {
+  nombre: string | null = '';
+  email: string | null = '';
+  balance: number = 0;
+
   constructor(private navCtrl: NavController) {}
 
   goBack() {
     this.navCtrl.navigateBack('/tabs/tab3'); // Regresar a la pestaña principal
+  }
+
+  ionViewWillEnter() {
+    this.nombre = localStorage.getItem('user_name');
+    this.email = localStorage.getItem('user_email');
+    const storedBalance = localStorage.getItem('user_balance');
+    this.balance = storedBalance ? parseFloat(storedBalance) : 0;
+  }
+
+  cerrarSesion() {
+    localStorage.clear();
+    this.navCtrl.navigateRoot('/login');
   }
 }
