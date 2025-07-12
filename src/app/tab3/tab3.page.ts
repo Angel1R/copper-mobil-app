07fe6055fbbbc34631a81059b8062dbf6747b425
Angel1R <angelrodrigo.ar55@gmail.com>
2025-07-11 20:54:03 -0600

    Actualizacion de Home

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-  standalone: false,
+  templateUrl: './tab3.page.html',
+  styleUrls: ['./tab3.page.scss'],
+  standalone: false
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  cargando: boolean = true;
 
   constructor() {}
 
+  async ngOnInit() {
+    // Aquí se puede agregar cualquier lógica que necesite cargar al iniciar
+    this.cargando = false;
+  }
 }
