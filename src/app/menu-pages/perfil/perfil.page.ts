42fb54cd9cd8ff8f9533b1a9371ee2a0c85e3a82
Angel1R <angelrodrigo.ar55@gmail.com>
2025-07-19 04:06:35 -0600

    Actualización de perfil

--- src/app/menu-pages/perfil/perfil.page.ts
+++ src/app/menu-pages/perfil/perfil.page.ts
@@ -5,28 +5,33 @@ import { NavController } from '@ionic/angular';
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
-  standalone:false
+  standalone: false
 })
 export class PerfilPage {
   nombre: string | null = '';
   email: string | null = '';
   balance: number = 0;
+  cargando: boolean = true;
 
   constructor(private navCtrl: NavController) {}
-
-  volverAHome() {
-    this.navCtrl.navigateBack('/tabs/tab3');
-    setTimeout(() => {
-      const activo = document.activeElement as HTMLElement;
-      activo?.blur();
-    }, 300); // evita conflicto con ion-searchbar
-  }
 
   ionViewWillEnter() {
     this.nombre = localStorage.getItem('user_name');
     this.email = localStorage.getItem('user_email');
     const storedBalance = localStorage.getItem('user_balance');
     this.balance = storedBalance ? parseFloat(storedBalance) : 0;
+
+    setTimeout(() => {
+      this.cargando = false;
+    }, 100);
+  }
+
+  volverAHome() {
+    this.navCtrl.navigateBack('/tabs/tab3');
+    setTimeout(() => {
+      const activo = document.activeElement as HTMLElement;
+      activo?.blur();
+    }, 300);
   }
 
   cerrarSesion() {
